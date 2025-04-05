b233b955831f637efa91548e11080435a5943400
nviorato <n.viorato@gmail.com>
2025-04-05 18:10:01 -0400

    Delete calculator.py

--- calculator.py
+++
@@ -1,8 +0,0 @@
-bill = float(input("Enter the bill amount: "))
-tip = float(input("Enter tip percentage: "))
-
-tip_amount = bill * (tip / 100)
-total = bill + tip_amount
-
-print(f"Tip: ${tip_amount}")
-print(f"Total: ${total}")
